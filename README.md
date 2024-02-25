2d7d4c2672153b7e3cbb353b6be3dc251b15e705
Christian M <chriamue@gmail.com>
2024-02-25 20:55:39 +0100

    :sparkles: adds esp32s3 feature

--- README.md
+++ README.md
@@ -6,13 +6,29 @@ This is an example of how to use the embassy library with an ESP32.
 
 ```bash
 cargo install cargo-espflash
+cargo install espup
+espup install
+. $HOME/export-esp.sh
 ```
+
+On Mac, if openssl does not work:
+
+```bash
+OPENSSL_NO_VENDOR=1 cargo install espup
+OPENSSL_NO_VENDOR=1 cargo install cargo-espflash
+```
+
 
 ## Flashing
 
 ```bash
 cargo espflash flash --release --monitor
 ```
+
+```bash
+cargo +esp run --no-default-features --features=esp32s3 --release --target xtensa-esp32s3-none-elf
+```
+
 
 ## Thanks to
 
--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,47 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+
+#[cfg(feature="esp32s3")]
+use esp32s3_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*};
+
+#[cfg(feature="esp32c3")]
 use esp32c3_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*};
+
 use esp_backtrace as _;
 use esp_println::print;
 
 #[embassy_executor::task]
-async fn one_second_task() {
-    let mut count = 0;
+async fn run() {
     loop {
-        esp_println::println!("Spawn Task Count: {}", count);
-        count += 1;
+        esp_println::println!("Hello world from embassy using esp-hal-async!");
         Timer::after(Duration::from_millis(1_000)).await;
     }
 }
 
 #[main]
 async fn main(spawner: Spawner) {
+    esp_println::println!("Init!");
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
+    #[cfg(feature = "embassy-time-systick")]
+    embassy::init(
+        &clocks,
+        esp32c3_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
+    );
+
+    #[cfg(feature = "embassy-time-timg0")]
     embassy::init(
         &clocks,
         esp32c3_hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks),
     );
 
-    spawner.spawn(one_second_task()).unwrap();
+    spawner.spawn(run()).ok();
 
-    // This line is for Wokwi only so that the console output is formatted correctly
-    print!("\x1b[20h");
-
-    let mut count = 0;
     loop {
-        esp_println::println!("Main Task Count: {}", count);
-        count += 1;
+        esp_println::println!("Bing!");
         Timer::after(Duration::from_millis(5_000)).await;
     }
 }
